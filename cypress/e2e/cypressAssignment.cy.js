7d90d52841897b0bc4c2f70db3c6baa6900f9a9a
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-06-30 16:56:43 +0500

    Update cypressAssignment.cy.js

--- cypress/e2e/cypressAssignment.cy.js
+++ cypress/e2e/cypressAssignment.cy.js
@@ -1,21 +1,13 @@
 describe("Login", () => {
   it("Enter user name & password", () => {
     cy.visit("https://opensource-demo.orangehrmlive.com");
-    cy.intercept(
-      {
-        method: "GET",
-        url: "https://jsonplaceholder.typicode.com/posts",
-      },
-      (req) => {
-        console.log(req.reply.Scopes[0]);
-      }
-    ).as("json");
-    cy.visit("https://jsonplaceholder.typicode.com/posts");
+    cy.intercept("GET", "/index.php/auth/validateCredentials").as("json");
 
     // cy.wait("@json");
     cy.get("#txtUsername").type("Admin");
     cy.get("#txtPassword").type("admin123");
-    // cy.get("#btnLogin").click();
+    cy.get("#btnLogin").click();
+    cy.wait("@json");
     // cy.get("#welcome").should("have.text", "Welcome mr.");
   });
 });
